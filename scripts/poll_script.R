7bf3b587dc9ab0e0c7ae2ff81a4bcfe77707945a
Conor Tompkins <conorotompkins@gmail.com>
2017-08-16 01:55:53 +0000

    created folders and project

---
+++ scripts/poll_script.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(lubridate)
+library(scales)
+
+URL <- "https://projects.fivethirtyeight.com/trump-approval-data/approval_polllist.csv"
+
+theme_set(theme_bw())
+
+set.seed(1234)
+
+my_data <- read_csv(URL) %>% 
+  mutate(startdate = mdy(startdate),
+         enddate = mdy(enddate))
+
+write_csv(my_data, paste("538_polls", Sys.Date()), ".csv")
+
+pollsters <- my_data %>% 
+  count(pollster, sort = TRUE) %>% 
+  select(pollster) %>% 
+  unlist()
+
+my_data %>% 
+  mutate(pollster = factor(pollster, levels = pollsters)) %>% 
+  ggplot(aes(pollster, weight)) +
+  geom_jitter(alpha = .25) +
+  coord_flip()
+
+df <- my_data %>% 
+  select(president, startdate, enddate, approve, disapprove, weight) %>% 
+  gather(key = poll_answer, value = poll_value, -c(president, startdate, enddate, weight))
+  
+group.colors <- c(approve = "#03bfc4", disapprove = "#f88179")
+
+df %>% 
+  mutate(poll_value = poll_value / 100) %>% 
+  ggplot(aes(startdate, poll_value, color = poll_answer, fill = poll_answer)) +
+  geom_hline(yintercept = .50) +
+  geom_segment(aes(x = startdate, xend = enddate, y = poll_value, yend = poll_value, alpha = weight)) +
+  geom_smooth(aes(weight = weight)) +
+  #geom_smooth(linetype = 2, se = FALSE) + unweighted geom_smooth
+  scale_color_manual(values = group.colors, labels = c("Approve", "Disapprove")) +
+  scale_fill_manual(values = group.colors, labels = c("Approve", "Disapprove")) +
+  scale_x_date(date_breaks = "month", date_labels = "%b %Y") +
+  scale_y_continuous(labels = percent_format()) +
+  guides(alpha = FALSE,
+         color = guide_legend(title = "Poll Response"),
+         fill = guide_legend(title = "Poll Response")) +
+  labs(title = "Donald Trump Approve Ratings",
+       x = NULL,
+       y = NULL)
+ggsave(paste0("images/", "Donald Trump Approval Ratings ", Sys.Date(), ".png"), width = 12, height = 9)
