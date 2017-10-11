0215cd61504298a6a495750c30719d1f0c9d824c
Conor Tompkins <conorotompkins@gmail.com>
2017-10-11 00:53:11 +0000

    cleaned up code, added save line

--- scripts/inset_plot.R
+++ scripts/inset_plot.R
@@ -1,6 +1,6 @@
 library(cowplot)
 
-
+#create main plot
 plot_1 <- df %>% 
   mutate(poll_value = poll_value / 100, 
          pollster = factor(pollster, levels = pollsters)) %>% 
@@ -12,19 +12,21 @@ plot_1 <- df %>%
   #geom_smooth(linetype = 2, se = FALSE) + #unweighted geom_smooth
   scale_color_manual(values = group.colors, labels = c("Approve", "Disapprove")) +
   scale_fill_manual(values = group.colors, labels = c("Approve", "Disapprove")) +
+  scale_alpha_continuous(range = c(.05, .4)) +
   scale_x_date(date_breaks = "week", date_labels = "%d %b") +
   scale_y_continuous(labels = percent_format()) +
-  coord_cartesian(xlim = c(ymd(Sys.Date() - 30), ymd(Sys.Date()))) +
+  coord_cartesian(xlim = c(ymd(Sys.Date() - 60), ymd(Sys.Date()))) +
   guides(alpha = FALSE,
          color = guide_legend(title = "Poll Response"),
          fill = guide_legend(title = "Poll Response")) +
   labs(title = "Donald Trump Approval Ratings",
-       subtitle = "Data from 538",
+       subtitle = "Past 60 Days",
        x = NULL,
        y = NULL,
-       caption = "@conor_tompkins") +
+       caption = "@conor_tompkins, Data from 538") +
   theme(legend.position = "bottom")
 
+#create inset plot
 plot_2 <- df %>% 
   mutate(poll_value = poll_value / 100, 
          pollster = factor(pollster, levels = pollsters)) %>% 
@@ -38,8 +40,7 @@ plot_2 <- df %>%
   scale_fill_manual(values = group.colors, labels = c("Approve", "Disapprove")) +
   #scale_x_date(date_breaks = "month", date_labels = "%b %Y") +
   scale_y_continuous(labels = percent_format(), 
-                     breaks = c(.25, .50, .75),
-                     limits = c(.2, .8)) +
+                     breaks = c(.4, .5, .6)) +
   guides(alpha = FALSE,
          color = FALSE,
          fill = FALSE) +
@@ -48,10 +49,12 @@ plot_2 <- df %>%
        x = NULL,
        y = NULL,
        caption = NULL) +
-  theme_minimal()
+  theme(axis.text.y = element_blank(),
+        axis.text.x = element_text(size = 12))
 
-#?draw_plot  
+  
 ggdraw() + 
   draw_plot(plot_1, 0, 0, 1, 1, 1) +
   draw_plot(plot_2, width = .7, height = 1, scale = .25, x = .55, y = .35)
 
+ggsave("images/inset_approval_ratings_plot.png")
